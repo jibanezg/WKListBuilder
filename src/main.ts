af4237842bac9aad91de23c78674682bb99acc47
Jhonathan Ibanez <bitbucket_ts@bitbucket.org>
2017-06-15 17:43:18 -0500

    Application enhanced to react.js

--- src/main.ts
+++
@@ -1,8 +0,0 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule } from './app/app.module';
-
-const bootstrapPromise =  platformBrowserDynamic().bootstrapModule(AppModule);
-
-//Logging bootstrap information
-bootstrapPromise.then(success => console.log(`Bootstrap success`))
-  .catch(err => console.error(err));
---
+++ wkarmybuilder/src/common/header/header.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import Nav from 'react-materialize';
+
+export default class Header extends Component {
+  render() {
+    return (
+      <div className="Bla">    
+        <Nav class="black">
+            <div class="nav-wrapper">
+                <ul id="nav-mobile" class="right hide-on-med-and-down">
+                    <li>
+                        blaaaa
+                    </li>
+                        
+                </ul>
+            </div>
+        </Nav>
+      </div>
+    );
+  }
+}
+
+
+
+
