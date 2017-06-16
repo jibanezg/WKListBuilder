5149893611a01365db30ed62ae9416b7aade2c1d
Jhonathan Ibanez <bitbucket_ts@bitbucket.org>
2017-06-16 18:04:58 -0500

    Adding display showhide point limit

--- wkarmybuilder/src/common/header/header.js
+++ wkarmybuilder/src/common/header/header.js
@@ -1,19 +1,47 @@
 import React, { Component } from 'react';
-import {Navbar} from 'react-materialize';
+import {Navbar, NavItem, Input} from 'react-materialize';
 
 export default class Header extends Component {
+
+  constructor(props) {
+    super(props); 
+    this.state = {
+      labelOn: true,
+      inputOn: false
+    }
+    this.handleClickOnPointLabel = this.handleClickOnPointLabel.bind(this);         
+  } 
+
+  showHidePointLimit(){
+    if(this.state.inputOn){
+      return (
+      <NavItem>
+        <Input placeholder="Point Limit" onClick={this.handleClickOnPointLabel}>
+        </Input>
+      </NavItem>);
+    }else if(this.state.labelOn){
+      return(
+      <NavItem className="hidden">
+        <Input className="white-text" defaultValue="/2000" disabled>
+        </Input>
+      </NavItem>);
+    }
+  }
+
+  toogleLabelAndInput(){
+    this.setState(!this.state.inputOn);
+    this.setState(!this.state.labelOn);
+  }
+
+  handleClickOnPointLabel(e){
+    this.toogleLabelAndInput();
+  }
+
   render() {
     return (
       <div className="Bla">    
-        <Navbar className="black">
-            <div class="nav-wrapper">
-                <ul id="nav-mobile" class="right hide-on-med-and-down">
-                    <li>
-                        blaaaa
-                    </li>
-                        
-                </ul>
-            </div>
+        <Navbar className="black" right>
+           {this.showHidePointLimit()} 
         </Navbar>
       </div>
     );
